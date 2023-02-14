22413d8ec8a1bc5b2f2744178a05cfc7caf6b4b9
Monir Hoshen <getmonirr@gmail.com>
2023-02-14 17:32:49 +0600

    key board shortcut added

--- script/textEditor.js
+++ script/textEditor.js
@@ -1,18 +1,21 @@
 // selection part
 const textArea = document.getElementById('text-area');
 const allTypesBtn = document.querySelectorAll('[data-type]');
+const keyboardShortcut = document.querySelectorAll('[data-boardKey]');
 const inputs = document.querySelectorAll('input');
+
+const textEditor = document.getElementById('text-editor');
 
 
 // function
 function typeHandle(e) {
-    const { dataset, attributes } = e.target;
+    const { dataset } = e.target;
     // textArea.style[dataset.type] = attributes.name.value;
     textArea.classList.toggle(dataset.type)
     e.target.classList.toggle('active')
 }
 
-function inputHandle (e) {
+function inputHandle(e) {
     textArea.style[e.target.dataset.type] = `${e.target.value}${e.target.dataset.type === 'color' ? '' : 'px'}`;
 }
 
@@ -21,6 +24,15 @@ allTypesBtn.forEach(btn => {
     btn.addEventListener('click', typeHandle);
 })
 
-inputs.forEach(input =>{
+inputs.forEach(input => {
     input.addEventListener('change', inputHandle);
 })
+// keyboard shortcut
+textEditor.addEventListener('keypress',(e) => {
+    keyboardShortcut.forEach(key => {
+        if(e.ctrlKey && e.code === key.dataset.boardkey){
+            textArea.classList.toggle(key.dataset.type)
+        }
+    });
+})
+
